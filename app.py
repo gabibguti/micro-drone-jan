c1a1556a5a858e21df5d6e5aa1b758badb708de6
costalonga <marcelo.costalonga.cardoso@gmail.com>
2019-11-13 15:17:17 -0300

    Fixed errors, server can now insert and find objects in database

--- app.py
+++ app.py
@@ -3,11 +3,21 @@ from pymongo import MongoClient
 from datetime import datetime
 import json
 
+from flask import jsonify
+
 app = Flask(__name__)
 
 client = MongoClient("localhost", 27017)
 db = client["smart_warehouse"]
 collection = db["packages"]
+
+# TODO add more status about the package
+# package:
+# {
+#     "id": <int>
+#     "date": <str: YYYY-MM-DD>
+#     "status": ["today", "delayed", "ok"]
+# }
 
 def update_all():
     # Update and get all packages
@@ -17,12 +27,12 @@ def update_all():
         if new_status != package["status"]:
             package["status"] = new_status
             # TODO use pymongo $set to update values, instead of adding new ones
-            # db.update({"id": package_id}, {"$set": package })
+            # db.update({"_id": package_id}, {"$set": package })
             collection.insert(package)
 
 def check_status(dt):
-    # FIXME use datetime.strptime / strftime
-    return "okay" if datetime(dt) > datetime.now() else "delayed"
+    date = datetime.strptime(dt, "%Y-%m-%d")
+    return "okay" if date > datetime.now() else "delayed"
 
 @app.route('/')
 def hello_world():
@@ -46,7 +56,7 @@ def add_package(package_id, date):
 def package_status(package_id):
     query = {"id": package_id}
     package = collection.find_one(query)
-
+    package.pop("_id", None)
     # update package status
     new_status = check_status(package["date"])
     if new_status != package["status"]:
@@ -54,7 +64,9 @@ def package_status(package_id):
         # TODO use pymongo $set to update values, instead of adding new ones
         # db.update({"id": package_id}, {"$set": package })
         collection.insert(package)
+    print(json.dumps(package))
     return json.dumps(package)
+    # return json.dumps(package)
 
 @app.route('/delayed_package')
 def get_delayed_package():
