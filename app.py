1642eeee489d638228970f2de126c90c6318cfd3
Gabriela Gutierrez <gabi_guti_1@hotmail.com>
2019-11-12 21:45:55 -0300

    Integrating app with html render

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return render_template('homepage.html')
 
 if __name__ == '__main__':
     app.run(5000, debug=True)
