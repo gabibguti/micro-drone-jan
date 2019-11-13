d7c140575208b0d5dfa9b6b386838ac803ec53e9
Gabriela Gutierrez <gabi_guti_1@hotmail.com>
2019-11-13 10:05:39 -0300

    Refactoring var name

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 def hello_world():
-  img = './static/shelves.svg'
-  return render_template('homepage.html', img=img)
+  SVG_EmptyShelf = './static/shelves.svg'
+  return render_template('homepage.html', SVG_EmptyShelf=SVG_EmptyShelf)
 
 if __name__ == '__main__':
     app.run(5000, debug=True)
