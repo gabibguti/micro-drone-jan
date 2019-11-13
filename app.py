7864f0b48b8ae6cf2ae0c6fdba8c3aec7138645a
Gabriela Gutierrez <gabi_guti_1@hotmail.com>
2019-11-13 11:23:11 -0300

    Adjusting shelf + pkg images

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__, template_folder='template')
 @app.route('/')
 def hello_world():
   SVG_EmptyShelf = './static/shelves.svg'
-  return render_template('homepage.html', SVG_EmptyShelf=SVG_EmptyShelf)
+  SVG_Package = './static/box_package_color_qrcode_f.svg'
+  return render_template('homepage.html', SVG_EmptyShelf=SVG_EmptyShelf, SVG_Package=SVG_Package)
 
 if __name__ == '__main__':
     app.run(5000, debug=True)
