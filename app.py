df547b9edcb06ed0d4596a05264b4bd82293dcf6
Gabriela Gutierrez <gabi_guti_1@hotmail.com>
2019-11-27 16:20:50 -0300

    Get packages on shelf from db

--- app.py
+++ app.py
@@ -33,29 +33,52 @@ def check_status(dt):
     date = datetime.strptime(dt, "%Y-%m-%d")
     return "okay" if date > datetime.now() else "delayed"
 
+def clear_db():
+  collection.remove({})
+
+def init_packages():
+  data = {}
+  for row in range(0, 2):
+    data[row] = {}
+    for col in range(0, 5):
+      data[row][col] = False
+  return data
+
+DATA_Packages = init_packages()
+
 @app.route('/')
 def hello_world():
+  print('##', DATA_Packages)
   SVG_EmptyShelf = './static/shelves.svg'
   SVG_Package = './static/box_package_color_qrcode_f.svg'
-  return render_template('homepage.html', SVG_EmptyShelf=SVG_EmptyShelf, SVG_Package=SVG_Package)
+  return render_template('homepage.html',
+  SVG_EmptyShelf=SVG_EmptyShelf,
+  SVG_Package=SVG_Package,
+  DATA_Packages=DATA_Packages)
 
-@app.route("/forward/", methods=['POST'])
-def move_forward():
-    #Moving forward code
-    forward_message = "Moving Forward..."
-    print(forward_message)
+@app.route("/refresh_shelf/", methods=['POST'])
+def refresh_shelf():
+    global DATA_Packages
+    print("\n\n***\n")
+    print("Refreshing shelf...")
+    raw_data = get_all_packages()
+    data = json.loads(raw_data)
+    DATA_Packages = init_packages()
+    for pkg in data:
+      DATA_Packages[pkg["row"]][pkg["col"]] = True
+    print(DATA_Packages)
+    print("\n***\n\n")
     return (''), 204
 
 @app.route('/all_packages')
 def get_all_packages():
-    update_all()
     all_packages = list(collection.find())
     for pkg in all_packages:
       pkg.pop("_id", None)
     return json.dumps(all_packages)
 
-@app.route('/add_package/<int:package_id>/<string:date>')
-def add_package(package_id, date):
+@app.route('/add_package/<int:package_id>/<string:date>/<int:row>/<int:col>')
+def add_package(package_id, date, row, col):
     # Note: data must be in the format: YYYY-MM-DD
     dt = date.split('-')
     year = int(dt[0])
@@ -64,7 +87,13 @@ def add_package(package_id, date):
     now = datetime.now()
     arrival = datetime(year, month, day)
     status = "okay" if arrival > now else "delayed"
-    package = {"id": package_id, "date": date, "status": status}
+    package = {
+      "id": package_id,
+      "date": date,
+      "status": status,
+      "row": row,
+      "col": col
+    }
     collection.insert(package)
     return 'Package info added to database'
 
