046fd42b8738adb4ff826ad1a247fe240290501e
Gabriela Gutierrez <gabi_guti_1@hotmail.com>
2019-12-04 16:08:50 -0300

    Melhorar debug das informacoes do qrcode

--- qrcode_reader.py
+++ qrcode_reader.py
@@ -42,8 +42,12 @@ def read_qrcode(img_name, render_inspection_img = False):
 		(x, y, w, h) = barcode.rect
 		cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 		# draw the barcode data and barcode type on the image
-		text = "{} ({})".format(barcodeData, barcodeType)
-		cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+		pos_x = 10
+		pos_y = 40
+		for line in lines:
+			text = "{}".format(line)
+			cv2.putText(image, text, (pos_x, pos_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+			pos_y += pos_y
 
 		cv2.namedWindow('Decoded QRCode', cv2.WINDOW_NORMAL)
 		cv2.resizeWindow('Decoded QRCode', 600, 600)
